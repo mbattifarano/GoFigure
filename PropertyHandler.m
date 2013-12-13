4798fb850904de849cfd3f2c58bdb2214b7106ec
osbornelab <matthew.battifarano@gmail.com>
2013-12-13 13:32:27 -0600

    added class for holding axis and figure rw properties

---
+++ PropertyHandler.m
@@ -0,0 +1,35 @@
+classdef PropertyHandler < handle
+    %PROPERTYHANDLER Holds 
+    %   Detailed explanation goes here
+    properties
+        figure
+        axes
+    end
+    properties (SetAccess = private)
+        readonly=struct('figure', {{'BeingDeleted','Children',...
+                                    'CurrentAxes','CurrentCharacter',...
+                                    'CurrentObject','Type'}},...
+                        'axes'  , {{'BeingDeleted','Children',...
+                                    'CurrentPoint','Parent',...
+                                    'TightInset','Title','Type',...
+                                    'XLabel','YLabel','ZLabel'}});
+        default_profile = 'default'
+    end
+    methods
+        function prophandler = PropertyHandler(fixed_props,profile)
+            if nargin == 1
+                profile = prophandler.default_profile;
+            end
+            prophandler.readonly.figure = horzcat(fixed_props',...
+                                           prophandler.readonly.figure);
+            props = load(['profiles/' profile '.mat']);
+            prophandler.figure = rmfield(props.figure,...
+                                   prophandler.readonly.figure);
+            prophandler.axes   = rmfield(props.axes,...
+                                   prophandler.readonly.axes);
+            
+        end
+    end
+    
+end
+
